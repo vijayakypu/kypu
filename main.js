d12eccbf5ee5a794e1bbccce21efd58e75cbedee
vijayakypu <kvlakshmi112233@gmail.com>
2018-07-07 15:04:38 +0530

    final project

--- main.js
+++ main.js
@@ -7,10 +7,58 @@ function getfile(file,callback){
       callback(xhr.responseText);
     }
   };
-  xhr.send(null);
+  xhr.send();
 }
 
 getfile("data.json",function(text){
   let data = JSON.parse(text);
   console.log(data);
+  career(data.career);
+  education(data.education);
+  skills(data.skills);
 })
+
+var child2=document.querySelector(".childTwo");
+function career(careerInfo){
+  var careerHeading=document.createElement("h3");
+  child2.appendChild(careerHeading);
+  careerHeading.textContent="career objective";
+
+  var careerHr=document.createElement("hr");
+  child2.appendChild(careerHr);
+
+  var ch=document.createElement("p");
+  ch.textContent=careerInfo.info;
+  child2.appendChild(ch);
+}
+
+function education(edu){
+  var eduHeading=document.createElement("h3");
+  eduHeading.textContent="Educational Qualifications";
+  child2.appendChild(eduHeading);
+
+  var careerHr=document.createElement("hr");
+  child2.appendChild(careerHr);
+
+  var eduTable=document.createElement("table");
+  eduTable.border="1";
+  var tr1="<tr><td>DEGREE</td><td>INSTITUTE</td><td>DATA</td></tr>";
+  eduTable.innerHTML=tr1;
+  var tr2="";
+  for(var i=0; i<edu.length; i++){
+    tr2+="<tr> <td>"+edu[i].degree+"</td> <td>"+edu[i].institute+"</td><td>"+edu[i].data+"</td></tr>";
+
+  }
+  eduTable.innerHTML=tr1+tr2;
+  child2.appendChild(eduTable);
+}
+
+function skills(skil){
+  var ul=document.createElement("ul");
+  child2.appendChild(ul);
+  for(var i=0; i<skil.length; i++){
+    var li=document.createElement("li");
+    li.textContent=skil[i].title+"::"+skil[i].content;
+    ul.appendChild(li);
+  }
+}
